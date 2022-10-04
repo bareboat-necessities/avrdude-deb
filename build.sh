e0008981811b6b12f881708108604a9c5d4abda1
mgrouch <mgrushinskiy@gmail.com>
2022-10-04 19:53:02 -0400

    initial

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+sudo apt-get -y install meson cmake make git \
+ dpkg-dev debhelper devscripts equivs pkg-config
+
+
+cd ~
+sudo apt-get -y install build-essential git cmake flex bison libelf-dev libusb-dev libhidapi-dev libftdi1-dev
+git clone https://github.com/avrdudes/avrdude
+cd avrdude
+cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -D HAVE_LINUXGPIO=1 -D HAVE_LINUXSPI=1 -B build_linux
+cd build_linux/
+make -j 4
+sudo make install
+cd ~
+
