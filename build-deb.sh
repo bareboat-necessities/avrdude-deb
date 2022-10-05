877eebaff33709e9a0571501f5ab5e7f29740277
mgrouch <mgrushinskiy@gmail.com>
2022-10-04 20:28:27 -0400

    initial

--- build-deb.sh
+++ build-deb.sh
@@ -3,4 +3,6 @@
 sudo apt-get -y install meson cmake make git \
  dpkg-dev debhelper devscripts equivs pkg-config
 
-sudo dpkg-buildpackage -b -d -uc -us
+sudo apt-get -y install build-essential git cmake flex bison libelf-dev libusb-dev libhidapi-dev libftdi1-dev
+
+dpkg-buildpackage -b -d -uc -us
