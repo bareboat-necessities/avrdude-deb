1c8a79f2e82d73b1ae3c32cfb57f5a532731f319
mgrouch <mgrushinskiy@gmail.com>
2022-10-04 20:34:15 -0400

    initial

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd avrdude
 cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -D HAVE_LINUXGPIO=1 -D HAVE_LINUXSPI=1 -B build_linux
 cd build_linux/
 make -j 4
-make install DESTDIR=./usr/local
+make install DESTDIR=../../debian/tmp/usr/local
 cd ../../
 
 
